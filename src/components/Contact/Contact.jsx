a4d0410ea52c849d44cff480e53a7cfb3048baaa
Prashant Kumar <programmerkrprashant@gmail.com>
2023-10-20 22:22:16 +0530

    name changed to upper case, message start with capital letter using react useRef, onChange

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,15 +4,44 @@ import './Contact.css'
 
 const Contact = () => {
     const form = useRef();
-    const nameRef = useRef(null);
-    const emailRef = useRef(null);
-    const messageRef = useRef(null);
+    let nameRef = useRef(null);
+    let emailRef = useRef(null);
+    let messageRef = useRef(null);
+
+    const nameHendlar = () => {
+     
+            let userName = nameRef.current.value.toUpperCase();
+            nameRef.current.value = userName;
+        
+    }
+
+    const messageHandler = () => {
+        const userMessage = messageRef.current.value;
+        const firstChar = userMessage.charAt(0).toUpperCase();
+        const restOfMessage = userMessage.slice(1);
+        messageRef.current.value = firstChar + restOfMessage;
+     }
+     
 
     const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs.sendForm('service_04h9oyt', 'template_hgw3cdm', form.current, 'PJBv1B-lysjDL9jhd')
             .then((result) => {
+
+
+                if (nameRef.current.value === '') {
+                    alert('Please fill your name')
+                    return false;
+                } if (emailRef.current.value === '') {
+                    alert('Please fill your email')
+                    return false;
+                }
+                if (messageRef.current.value === '') {
+                    alert('Please Write Your Message');
+                    return false;
+                }
+
                 alert("sent message successfuly");
                 console.log(result)
                 // e.target.reset(); // Clear the form fields
@@ -24,6 +53,9 @@ const Contact = () => {
             }, (error) => {
                 alert("sending message fialled");
                 console.log(error)
+                nameRef.current.value = '';
+                emailRef.current.value = '';
+                messageRef.current.value = '';
             });
 
     };
@@ -43,16 +75,22 @@ const Contact = () => {
                 <div className="form-box">
                     <h2>Contact me</h2>
                     <label htmlFor="name">Name
-                        <input type="text" name="name" id="name" placeholder="enter name..." ref={nameRef} required />
+                        <input type="text" name="name" id="name" placeholder="enter name..." ref={nameRef} onChange={nameHendlar} />
                     </label>
 
 
                     <label id="email-label">Email
-                        <input type="email" placeholder="enter email..." ref={emailRef} name="email" required />
+                        <input type="email" placeholder="enter email..." ref={emailRef} name="email" />
                     </label>
 
-                    <label htmlFor="message">message
-                        <textarea name="message" placeholder='type message here' rows="3" cols="30" ref={messageRef}required></textarea>
+                    <label htmlFor="message">Message
+                        <textarea name="message"
+                            placeholder='type message here'
+                            rows="3"
+                            cols="30"
+                            ref={messageRef}
+                            onChange={messageHandler}
+                        ></textarea>
                     </label>
 
                     <div className="btn-gurup">
